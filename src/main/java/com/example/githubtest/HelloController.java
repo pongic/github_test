24a190da4486df7c4cbcbd589fee02f33bcd49b2
pongic <pongic@naver.com>
2022-12-16 14:25:46 +0900

    commit

---
+++ src/main/java/com/example/githubtest/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.githubtest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+  @GetMapping("/")
+  public String helloWorld() {
+    return "To-do Application!";
+  }
+}
